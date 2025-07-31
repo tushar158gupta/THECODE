f1886d5668a2f91dd038295a7125d2ae3094402e
Tushar <tushar158gupta@gmail.com>
2025-07-31 22:26:13 +0530

    wallet part 2

--- remix code/wallet.sol
+++ remix code/wallet.sol
@@ -5,16 +5,23 @@ pragma solidity >=0.8.2 <0.9.0;
 contract Storage {
 
 address public owner;
+string public fallstring;
 
 constructor(){
     owner = msg.sender;
 }
 
+//  a modifier
 modifier onlyowner(){
     require (msg.sender == owner , "You are not the owner");
     _;
 }
 
+// Events rae used to maintain the logs
+event tusharevent(address personwhoranfucntion , uint etheramt);
+
+
+event questionevent(address senderaddress , address recieveraddress , uint etheramt);
 
 // ONLY CHECKING THE BALANCE==================================
 // the contracts
@@ -38,7 +45,6 @@ return owner.balance;
 
 // user -> contract
 function SendToContract() external payable{
-
 }
 
 // contract -> owner
@@ -52,12 +58,32 @@ require(address(this).balance > amtinwwi  , "Contract do not have enough ether t
 recipient.transfer(amtinwwi);
 }
 
-// oneuser -> anotheruser
+// oneuser -> anotheruser ( caputuring the value from the owner(sender) )
 function SendMoneyToAddressViaAddress(address payable recipient) public payable{
     require(msg.sender.balance>msg.value , "not have enough balance");
     recipient.transfer(msg.value);
+    emit questionevent(msg.sender , recipient , msg.value);
 }
 
+// user -> owner 
+function SendMoneyToOwnerViaUser() public payable {
+    require (msg.value>0 , "not enough wie");
+    payable (owner).transfer(msg.value);
 
+}
+// this was necessary fotr a fallback to work and used to send the mether to the contract without any function is called using empty calldata
+receive() external payable {
+
+// emitting the Event can be used in any fucntion to capture the logs
+emit tusharevent(msg.sender, msg.value);
+
+
+ }
+
+// when a wrong fucntion that does not even exist is called but the user has tried to send some money then the money will be sent to the contract.
+fallback() external payable   { 
+    fallstring = "the function has been fallen back";
+
+}
    
 }
